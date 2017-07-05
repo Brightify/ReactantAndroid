3e9e17be683436f102c1805eb186d7ef4e1d587a
Filip Dolník <filip.dolnik.96@gmail.com>
2017-07-05 13:25:09 +0200

    Fix makeGuard.

--- Core/src/main/kotlin/org/brightify/reactant/core/make.kt
+++ Core/src/main/kotlin/org/brightify/reactant/core/make.kt
@@ -3,28 +3,37 @@ package org.brightify.reactant.core
 import android.content.Context
 import android.view.View
 import org.brightify.reactant.core.component.Component
-import java.util.concurrent.ConcurrentHashMap
 
 /**
  *  @author <a href="mailto:filip.dolnik.96@gmail.com">Filip Dolnik</a>
  */
 typealias FactoryWithContext<T> = (Context) -> T
 
-private var makeUsed = ConcurrentHashMap<Thread, Boolean>()
+private var makeUsed = ThreadLocal<Int>()
 
-class MakeNotUsedException: RuntimeException("Component must be initialized using make().")
+class MakeNotUsedException: RuntimeException("Some Component was not initialized using make().")
 
-fun makeGuard() {
-    if (makeUsed[Thread.currentThread()] != true) {
+class MakeGuardNotUsedException: RuntimeException("Some Component did not call makeGuard() in its init.")
+
+fun Component<*, *>.makeGuard() {
+    if (makeUsed.get() == null || makeUsed.get() < 1) {
         throw MakeNotUsedException()
     }
+    makeUsed.set(makeUsed.get() - 1)
 }
 
 fun <T> make(factory: () -> T): T {
-    makeUsed[Thread.currentThread()] = true
+    val makeUsedCount = makeUsed.get() ?: 0
+    makeUsed.set(makeUsedCount + 1)
     return factory().also {
-        (it as? Component<*, *>)?.init()
-        makeUsed[Thread.currentThread()] = false
+        if (it is Component<*, *>) {
+            if (makeUsed.get() != makeUsedCount) {
+                throw MakeGuardNotUsedException()
+            }
+            it.init()
+        } else {
+            makeUsed.set(makeUsedCount)
+        }
     }
 }
 
@@ -83,10 +92,17 @@ fun <P1, P2, P3, P4, T> make(factory: (P1, P2, P3, P4, Context) -> T, p1: P1, p2
 }
 
 fun <T> Context.make(factory: (Context) -> T): T {
-    makeUsed[Thread.currentThread()] = true
+    val makeUsedCount = makeUsed.get() ?: 0
+    makeUsed.set(makeUsedCount + 1)
     return factory(this).also {
-        (it as? Component<*, *>)?.init()
-        makeUsed[Thread.currentThread()] = false
+        if (it is Component<*, *>) {
+            if (makeUsed.get() != makeUsedCount) {
+                throw MakeGuardNotUsedException()
+            }
+            it.init()
+        } else {
+            makeUsed.set(makeUsedCount)
+        }
     }
 }
 
