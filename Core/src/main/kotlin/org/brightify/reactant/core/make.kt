660e8a48b39cdc4fcca738615df8e323e2b2339c
Matous Hybl <hyblmatous@gmail.com>
2017-07-06 15:02:14 +0200

    Try to fix problem with Unit componentState (unsuccessfully).

--- Core/src/main/kotlin/org/brightify/reactant/core/make.kt
+++ Core/src/main/kotlin/org/brightify/reactant/core/make.kt
@@ -1,8 +1,11 @@
 package org.brightify.reactant.core
 
 import android.content.Context
+import android.util.Log
 import android.view.View
 import org.brightify.reactant.core.component.Component
+import org.brightify.reactant.core.component.setComponentState
+import java.util.*
 
 /**
  *  @author <a href="mailto:filip.dolnik.96@gmail.com">Filip Dolnik</a>
@@ -37,10 +40,18 @@ fun <T> make(factory: () -> T): T {
     }
 }
 
+inline fun <T: Component<STATE, *>, reified STATE> makeComponent(noinline factory: () -> T): T {
+    return make(factory).also { prepareComponent(it, STATE::class.java) }
+}
+
 fun <P1, T> make(factory: (P1) -> T, p1: P1): T {
     return make { ->
         factory(p1)
     }
+}
+
+inline fun <P1, T: Component<STATE, *>, reified STATE> makeComponent(noinline factory: (P1) -> T, p1: P1): T {
+    return make(factory, p1).also { prepareComponent(it, STATE::class.java) }
 }
 
 fun <P1, P2, T> make(factory: (P1, P2) -> T, p1: P1, p2: P2): T {
@@ -49,10 +60,18 @@ fun <P1, P2, T> make(factory: (P1, P2) -> T, p1: P1, p2: P2): T {
     }
 }
 
+inline fun <P1, P2, T: Component<STATE, *>, reified STATE> makeComponent(noinline factory: (P1, P2) -> T, p1: P1, p2: P2): T {
+    return make(factory, p1, p2).also { prepareComponent(it, STATE::class.java) }
+}
+
 fun <P1, P2, P3, T> make(factory: (P1, P2, P3) -> T, p1: P1, p2: P2, p3: P3): T {
     return make { ->
         factory(p1, p2, p3)
     }
+}
+
+inline fun <P1, P2, P3, T: Component<STATE, *>, reified STATE> makeComponent(noinline factory: (P1, P2, P3) -> T, p1: P1, p2: P2, p3: P3): T {
+    return make(factory, p1, p2, p3).also { prepareComponent(it, STATE::class.java) }
 }
 
 fun <P1, P2, P3, P4, T> make(factory: (P1, P2, P3, P4) -> T, p1: P1, p2: P2, p3: P3, p4: P4): T {
@@ -61,9 +80,19 @@ fun <P1, P2, P3, P4, T> make(factory: (P1, P2, P3, P4) -> T, p1: P1, p2: P2, p3:
     }
 }
 
+inline fun <P1, P2, P3, P4, T: Component<STATE, *>, reified STATE> makeComponent(noinline factory: (P1, P2, P3, P4) -> T, p1: P1, p2: P2, p3: P3, p4: P4): T {
+    return make(factory, p1, p2, p3, p4).also { prepareComponent(it, STATE::class.java) }
+}
+
 fun <T> make(factory: (Context) -> T): FactoryWithContext<T> {
     return { context: Context ->
         context.make(factory)
+    }
+}
+
+inline fun <T: Component<STATE, *>, reified STATE> makeComponent(noinline factory: (Context) -> T): FactoryWithContext<T> {
+    return { context: Context ->
+        context.make(factory).also { prepareComponent(it, STATE::class.java) }
     }
 }
 
@@ -73,9 +102,21 @@ fun <P1, T> make(factory: (P1, Context) -> T, p1: P1): FactoryWithContext<T> {
     }
 }
 
+inline fun <P1, T: Component<STATE, *>, reified STATE> makeComponent(noinline factory: (P1, Context) -> T, p1: P1): FactoryWithContext<T> {
+    return { context: Context ->
+        context.make(factory, p1).also { prepareComponent(it, STATE::class.java) }
+    }
+}
+
 fun <P1, P2, T> make(factory: (P1, P2, Context) -> T, p1: P1, p2: P2): FactoryWithContext<T> {
     return { context: Context ->
         context.make(factory, p1, p2)
+    }
+}
+
+inline fun <P1, P2, T: Component<STATE, *>, reified STATE> makeComponent(noinline factory: (P1, P2, Context) -> T, p1: P1, p2: P2): FactoryWithContext<T> {
+    return { context: Context ->
+        context.make(factory, p1, p2).also { prepareComponent(it, STATE::class.java) }
     }
 }
 
@@ -85,67 +126,109 @@ fun <P1, P2, P3, T> make(factory: (P1, P2, P3, Context) -> T, p1: P1, p2: P2, p3
     }
 }
 
+inline fun <P1, P2, P3, T: Component<STATE, *>, reified STATE> makeComponent(noinline factory: (P1, P2, P3, Context) -> T, p1: P1, p2: P2, p3: P3): FactoryWithContext<T> {
+    return { context: Context ->
+        context.make(factory, p1, p2, p3).also { prepareComponent(it, STATE::class.java) }
+    }
+}
+
 fun <P1, P2, P3, P4, T> make(factory: (P1, P2, P3, P4, Context) -> T, p1: P1, p2: P2, p3: P3, p4: P4): FactoryWithContext<T> {
     return { context: Context ->
         context.make(factory, p1, p2, p3, p4)
     }
 }
 
-fun <T> Context.make(factory: (Context) -> T): T {
-    val makeUsedCount = makeUsed.get() ?: 0
-    makeUsed.set(makeUsedCount + 1)
-    return factory(this).also {
-        if (it is Component<*, *>) {
-            if (makeUsed.get() != makeUsedCount) {
-                throw MakeGuardNotUsedException()
-            }
-            it.init()
-        } else {
-            makeUsed.set(makeUsedCount)
-        }
+inline fun <P1, P2, P3, P4, T: Component<STATE, *>, reified STATE> makeComponent(noinline factory: (P1, P2, P3, P4, Context) -> T, p1: P1, p2: P2, p3: P3, p4: P4): FactoryWithContext<T> {
+    return { context: Context ->
+        context.make(factory, p1, p2, p3, p4).also { prepareComponent(it, STATE::class.java) }
     }
+}
+
+fun <T> Context.make(factory: (Context) -> T): T {
+    return make { -> factory(this) }
+}
+
+inline fun <T: Component<STATE, *>, reified STATE> Context.makeComponent(noinline factory: (Context) -> T): T {
+    return makeComponent { -> factory(this) }
 }
 
 fun <P1, T> Context.make(factory: (P1, Context) -> T, p1: P1): T {
-    return make { context: Context ->
-        factory(p1, context)
-    }
+    return make { -> factory(p1, this) }
+}
+
+inline fun <P1, T: Component<STATE, *>, reified STATE> Context.makeComponent(noinline factory: (P1, Context) -> T, p1: P1): T {
+    return makeComponent { factory(p1, this) }
 }
 
 fun <P1, P2, T> Context.make(factory: (P1, P2, Context) -> T, p1: P1, p2: P2): T {
-    return make { context: Context ->
-        factory(p1, p2, context)
-    }
+    return make { -> factory(p1, p2, this) }
+}
+
+inline fun <P1, P2, T: Component<STATE, *>, reified STATE> Context.makeComponent(noinline factory: (P1, P2, Context) -> T, p1: P1, p2: P2): T {
+    return makeComponent { -> factory(p1, p2, this) }
 }
 
 fun <P1, P2, P3, T> Context.make(factory: (P1, P2, P3, Context) -> T, p1: P1, p2: P2, p3: P3): T {
-    return make { context: Context ->
-        factory(p1, p2, p3, context)
-    }
+    return make { -> factory(p1, p2, p3, this) }
+}
+
+inline fun <P1, P2, P3, T: Component<STATE, *>, reified STATE> Context.makeComponent(noinline factory: (P1, P2, P3, Context) -> T, p1: P1, p2: P2, p3: P3): T {
+    return makeComponent { -> factory(p1, p2, p3, this) }
 }
 
 fun <P1, P2, P3, P4, T> Context.make(factory: (P1, P2, P3, P4, Context) -> T, p1: P1, p2: P2, p3: P3, p4: P4): T {
-    return make { context: Context ->
-        factory(p1, p2, p3, p4, context)
-    }
+    return make { -> factory(p1, p2, p3, p4, this) }
+}
+
+inline fun <P1, P2, P3, P4, T: Component<STATE, *>, reified STATE> Context.makeComponent(noinline factory: (P1, P2, P3, P4, Context) -> T, p1: P1, p2: P2, p3: P3, p4: P4): T {
+    return makeComponent { -> factory(p1, p2, p3, p4, this) }
 }
 
 fun <T> View.make(factory: (Context) -> T): T {
     return context.make(factory)
 }
 
+inline fun <T: Component<STATE, *>, reified STATE> View.makeComponent(noinline factory: (Context) -> T): T {
+    return context.makeComponent(factory)
+}
+
 fun <P1, T> View.make(factory: (P1, Context) -> T, p1: P1): T {
     return context.make(factory, p1)
+}
+
+inline fun <P1, T: Component<STATE, *>, reified STATE> View.makeComponent(noinline factory: (P1, Context) -> T, p1: P1): T {
+    return context.makeComponent(factory, p1)
 }
 
 fun <P1, P2, T> View.make(factory: (P1, P2, Context) -> T, p1: P1, p2: P2): T {
     return context.make(factory, p1, p2)
 }
 
+inline fun <P1, P2, T: Component<STATE, *>, reified STATE> View.makeComponent(noinline factory: (P1, P2, Context) -> T, p1: P1, p2: P2): T {
+    return context.makeComponent(factory, p1, p2)
+}
+
 fun <P1, P2, P3, T> View.make(factory: (P1, P2, P3, Context) -> T, p1: P1, p2: P2, p3: P3): T {
     return context.make(factory, p1, p2, p3)
 }
 
+inline fun <P1, P2, P3, T: Component<STATE, *>, reified STATE> View.makeComponent(noinline factory: (P1, P2, P3, Context) -> T, p1: P1, p2: P2, p3: P3): T {
+    return context.makeComponent(factory, p1, p2, p3)
+}
+
 fun <P1, P2, P3, P4, T> View.make(factory: (P1, P2, P3, P4, Context) -> T, p1: P1, p2: P2, p3: P3, p4: P4): T {
     return context.make(factory, p1, p2, p3, p4)
+}
+
+inline fun <P1, P2, P3, P4, T: Component<STATE, *>, reified STATE> View.makeComponent(noinline factory: (P1, P2, P3, P4, Context) -> T, p1: P1, p2: P2, p3: P3, p4: P4): T {
+    return context.makeComponent(factory, p1, p2, p3, p4)
+}
+
+fun <STATE> prepareComponent(component: Component<STATE, *>, stateClass: Class<STATE>) {
+    Log.d("AAA", "AAA $component")
+    if (stateClass == Unit::class) {
+        Log.d("BBB", "BBB $component")
+        @Suppress("UNCHECKED_CAST")
+        component.componentState = Unit as STATE
+    }
 }
